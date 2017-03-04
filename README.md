ace7452828a2d27bf3d6de29ec49c69988dfe7a2
Unknown <tmlohnes@shaw.ca>
2017-03-03 21:21:50 -0800

    Updated create script, and powershell script and readme.md

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # HC_DPD-SQLite
 This a personal project to "automate" the download of the latest Health Canada Drug Product Data Extracts (i.e. allfiles.zip) and import them into an SQLite database for use.  My personal preference is to focus on python and php for "front-end" use of this data.  
 
-*Note:* You must ensure you have SQLite3 Installed. 
+*Note:* You must ensure you have SQLite3 Installed.
 
 ### For Linux (Ubuntu)
 ```
-sudo apt-get install sqlite3 
+sudo apt-get install sqlite3
 ```
 
 ### For Linux (Arch)
@@ -13,3 +13,8 @@ See the [Arch Wiki](https://wiki.archlinux.org/index.php/SQLite)
 
 ### For Windows
 Download the the sqlite tools from here: https://sqlite.org/download.html under the Precompiled Binaries for Windows section.
+
+### Execute
+```
+sqlite3 -init create_dpd_db.sql dpd.db
+```
--- getDPD-powershell.ps1
+++ getDPD-powershell.ps1
@@ -1,9 +1,10 @@
 ﻿# obtain single file
 #wget "http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/drug.zip" -OutFile "drug.zip"
-
+# Get present location
+$currentDir = Get-Location
 # Obtain all files
-$dpdAllFiles = "allfiles.zip"
-wget ("http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/" + $dpdAllFiles) -OutFile $dpdAllFiles
+$dpd_allfiles = "allfiles.zip"
+wget ("http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/" + $dpd_allfiles) -OutFile $dpd_allfiles
 # http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/allfiles.zip
 
 # Unzip the file
@@ -16,10 +17,10 @@ function Unzip
 }
 
 # Unzip the archive
-Unzip "C:\Dev\$($dpdAllFiles)" "C:\Dev\"
+Unzip $currentDir"\$($dpd_allfiles)" $currentDir"\"
 
 # Remove the zip archive
-Remove-Item $dpdAllFiles
+Remove-Item $dpd_allfiles
 
 # Dir Listing after file received
 Get-ChildItem .\
